1db32dc2576bc1333f0a7dcfd2811b4a94a10f61
Dane Solberg <danesolberg@gmail.com>
2022-08-12 13:59:15 -0400

    Hide search bar until click

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,22 +1,31 @@
 import SearchBar from "./SearchBar";
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 
 const Banner = () => {
+  const [searchVisible, setSearchVisible] = useState(false);
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div
           style={{
+            height: "50px",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
           }}
         >
-          <span id="get-part">A place to get</span>
-          <SearchBar />
-          <span> the cool stuff.</span>
+          <span>A place to&nbsp;</span>
+          <span
+            id="get-part"
+            onClick={() => setSearchVisible((prevValue) => !prevValue)}
+          >
+            get
+          </span>
+          {searchVisible ? <SearchBar /> : null}
+          <span>the cool stuff.</span>
         </div>
       </div>
     </div>
